b17708fa166ba50a9331518c50528b04c72e0af8
Mykezero <Mykezero@users.noreply.github.com>
2019-01-29 23:04:41 -0500

    Add FFXI packet structure

--- client.c
+++ client.c
@@ -1,7 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <arpa/inet.h>
+#include <stdint.h>
+
+/* 
+ * Packet Structure
+ * https://wiki.dspt.info/index.php/Packet_Header
+ */
+struct header
+{
+	uint8_t id;
+	uint8_t size;
+	uint16_t sync;
+	uint8_t payload[12];
+};
 
 int main(void)
 {
+	struct header header;
+	header.id = 1;
+	header.size = 8;
+	header.sync = 0;
+	header.payload[0] = 10;
 }
